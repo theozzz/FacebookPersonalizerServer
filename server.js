a5e23885ea4352c69409738f0d288dd4512b3c99
Theo <thcha@smile.fr>
2015-06-17 16:57:07 +0200

    trying imgmagic

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.configure(function(){
 });
 
 app.get('/test', function(req, res){
-    console.log(img);
     im.convert(['./images/7lRgXhS3Alc.png', '-resize', '25x120', 'kittens-small.jpg'],function(err, stdout){
         if (err) throw err;
         console.log('stdout:', stdout);
