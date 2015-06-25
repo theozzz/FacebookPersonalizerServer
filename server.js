4bbf9da6a2a35ecde1e82be9e4f8a79af166b9ac
Theo <thcha@smile.fr>
2015-06-25 16:24:00 +0200

    ok

--- server.js
+++ server.js
@@ -87,7 +87,6 @@ app.post('/set-img', function(req, res){
             im.convert(['./uploaded-images/' + imgNameArray[i], '-fill', 'red', '-tint', '100%', imgNameArray[i]],
                 function (err, stdout) {
                     if (err) throw err;
-                    callback();
                 });
         });
     }
