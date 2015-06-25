67b8be5d4eb7f18e1dd22627f74f7f94d9f3cf26
Theo <thcha@smile.fr>
2015-06-25 15:28:57 +0200

    ok

--- server.js
+++ server.js
@@ -115,8 +115,8 @@ app.post('/set-img', function(req, res){
 });
 var download = function(uri, filename, callback){
     request.head(uri, function(err, res, body){
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
+        /*console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);*/
 
         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
